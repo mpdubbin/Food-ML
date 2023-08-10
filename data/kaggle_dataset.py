87a159ac8de42baaea3740ccc3e4a5dd760744c7
Michael Dubbin <mpdubbin@gmail.com>
2023-08-10 18:26:52 -0400

    Script to download Kaggle dataset; downloaded Kaggle dataset

---
+++ data/kaggle_dataset.py
@@ -0,0 +1,13 @@
+import kaggle
+
+# Authenticate using your API token
+kaggle.api.authenticate()
+
+# Specify the dataset you want to download
+dataset_name = 'fda/adverse-food-events'
+
+# Set the path where you want to download and unzip the dataset
+download_path = './data/'
+
+# Download and unzip the dataset
+kaggle.api.dataset_download_files(dataset_name, path=download_path, unzip=True)
