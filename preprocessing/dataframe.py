4fae23ae2aa615cc567d0b85ce074eca0d028c94
Michael Dubbin <mpdubbin@gmail.com>
2023-08-10 18:45:21 -0400

    Commented dataframe function

--- preprocessing/dataframe.py
+++ preprocessing/dataframe.py
@@ -2,5 +2,8 @@ import pandas as pd
 
 
 def dataframe():
+    """
+    Function to read the kaggle csv into a dataframe; function so that the dataframe doesn't need to be stored and can be accessed in-memory.
+    """
     df = pd.read_csv("data/CAERS_ASCII_2004_2017Q2.csv")
     return df
