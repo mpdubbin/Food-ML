9b981fdbb86dc55da7c103cc56844bc57385e317
Michael Dubbin <mpdubbin@gmail.com>
2023-08-14 20:52:32 -0400

    Corrected file path to data/forecast_data

--- preprocessing/dataframe.py
+++ preprocessing/dataframe.py
@@ -5,7 +5,7 @@ def fulfillment_center_info():
     """
     Function to put the fulfillment center data into a pandas dataframe
     """
-    df = pd.read_csv("data/fulfilment_center_info.csv")
+    df = pd.read_csv("data/forecast_data/fulfilment_center_info.csv")
     return df
 
 
@@ -13,7 +13,7 @@ def meal_info():
     """
     Function to put the meal info data into a pandas dataframe
     """
-    df = pd.read_csv("data/meal_info.csv")
+    df = pd.read_csv("data/forecast_data/meal_info.csv")
     return df
 
 
@@ -21,7 +21,7 @@ def sample_submission():
     """
     Function to put the sample submission data into a pandas dataframe
     """
-    df = pd.read_csv("data/sample_submission.csv")
+    df = pd.read_csv("data/forecast_data/sample_submission.csv")
     return df
 
 
@@ -29,7 +29,7 @@ def test_data():
     """
     Function to put the test data into a pandas dataframe
     """
-    df = pd.read_csv("data/test.csv")
+    df = pd.read_csv("data/forecast_data/test.csv")
     return df
 
 
@@ -37,5 +37,7 @@ def train_data():
     """
     Function to put the training data into a pandas dataframe
     """
-    df = pd.read_csv("data/train.csv")
+    df = pd.read_csv("data/forecast_data/train.csv")
     return df
+
+print(fulfillment_center_info())
