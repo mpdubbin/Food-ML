59c18d3dd0f30bf2778413b6741021afd014e4af
Michael Dubbin <mpdubbin@gmail.com>
2023-08-14 20:32:47 -0400

    Separate functions for each csv file to use as modules

--- preprocessing/dataframe.py
+++ preprocessing/dataframe.py
@@ -1,9 +1,41 @@
 import pandas as pd
 
 
-def dataframe():
+def fulfillment_center_info():
     """
-    Function to read the kaggle csv into a dataframe; function so that the dataframe doesn't need to be stored and can be accessed in-memory.
+    Function put the fulfillment center data into a pandas dataframe
     """
-    df = pd.read_csv("data/CAERS_ASCII_2004_2017Q2.csv")
+    df = pd.read_csv("data/fulfilment_center_info.csv")
+    return df
+
+
+def meal_info():
+    """
+    Function put the meal info data into a pandas dataframe
+    """
+    df = pd.read_csv("data/meal_info.csv")
+    return df
+
+
+def sample_submission():
+    """
+    Function put the sample submission data into a pandas dataframe
+    """
+    df = pd.read_csv("data/sample_submission.csv")
+    return df
+
+
+def test_data():
+    """
+    Function put the test data into a pandas dataframe
+    """
+    df = pd.read_csv("data/test.csv")
+    return df
+
+
+def train_data():
+    """
+    Function put the training data into a pandas dataframe
+    """
+    df = pd.read_csv("data/train.csv")
     return df
